482d9fd0b06b0fb2eaea726a8ba4ea9f074cb64e
Lisa Hamm <lisalhamm@gmail.com>
2015-04-30 16:00:03 -0500

    Initial commit of RequestHandler test

---
+++ test/com/lisahamm/RequestHandlerTest.java
@@ -0,0 +1,15 @@
+package com.lisahamm;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class RequestHandlerTest {
+    
+    @Test
+    public void testParseRequestWithOnlyRequestLine() throws Exception {
+        String httpRequest = "GET / HTTP/1.1";
+        RequestHandler requestHandler = new RequestHandler(httpRequest);
+        requestHandler.parseRequest();
+        assertEquals(requestHandler.requestLine, httpRequest);
+    }
+}
