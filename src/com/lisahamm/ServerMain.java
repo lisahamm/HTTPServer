5ebb623f80b58324c3ba8eb38b799baa0497360d
Lisa Hamm <lisalhamm@gmail.com>
2015-04-30 15:58:55 -0500

    Refactor of main class

--- src/com/lisahamm/ServerMain.java
+++ src/com/lisahamm/ServerMain.java
@@ -6,8 +6,7 @@ import java.util.Date;
 
 public class ServerMain {
 
-
-    public static void main(String[] args) throws IOException {
+    public void main(String[] args) throws IOException {
 
         int portNumber;
         if (args.length != 1) {
@@ -15,12 +14,13 @@ public class ServerMain {
         } else {
             portNumber = Integer.parseInt(args[0]);
         }
-        
-        try (
-             ServerSocket serverSocket = new ServerSocket(portNumber)
-        ) {
-            System.out.println("Server is listening on port: " + portNumber);
-            while (true) {
+
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+        System.out.println("Server is listening on port: " + portNumber);
+        boolean running = true;
+
+        try {
+            while (running) {
                 Socket clientSocket = serverSocket.accept();
                 PrintWriter out =
                         new PrintWriter(clientSocket.getOutputStream(), true);
@@ -29,17 +29,17 @@ public class ServerMain {
 
                 System.out.println("Connection made with " + clientSocket);
 
-                String inputLine = in.readLine();
-                System.out.println(inputLine);
+                String httpRequest = in.readLine();
+                System.out.println(httpRequest);
 
-                if (inputLine.contains("GET /")) {
+                if (httpRequest.contains("GET /")) {
                     out.flush();
                     out.write("HTTP/1.1 200 OK\r\n");
                     out.write("Content-Type: text/plain\r\n");
                     out.write("Date: " + new Date() + "\r\n");
                     out.write("\r\n");
                     out.flush();
-                } else if (inputLine.contains("POST /")) {
+                } else if (httpRequest.contains("POST /")) {
                     out.flush();
                     out.write("HTTP/1.1 200 OK\r\n");
                     out.write("Content-Type: text/plain\r\n");
@@ -57,6 +57,8 @@ public class ServerMain {
             System.out.println("Exception caught when trying to listen on port "
                     + portNumber + " or listening for a connection");
             System.out.println(e.getMessage());
+        } finally {
+            serverSocket.close();
         }
     }
 }
