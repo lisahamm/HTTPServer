f4374957afaffdec940b088d8cb53aeed398bf3d
Lisa Hamm <lisalhamm@gmail.com>
2015-05-01 10:57:51 -0500

    Renamed RequestHandler class to RequestParser, which now parses a request into request line, headers, and body strings

---
+++ src/com/lisahamm/RequestParser.java
@@ -0,0 +1,40 @@
+package com.lisahamm;
+
+
+public class RequestParser {
+    private String httpRequest;
+    private String requestLine;
+    private String headers;
+    private String body;
+
+    public RequestParser(String httpRequest) {
+        this.httpRequest = httpRequest;
+    }
+
+    public void parseRequest() {
+        String request = httpRequest;
+        String[] messageComponents = httpRequest.split("\r\n\r\n");
+        if (messageComponents.length > 1) {
+            body = messageComponents[1];
+            request = messageComponents[0];
+        }
+        messageComponents = request.split("\r\n", 2);
+        requestLine = messageComponents[0];
+        if (messageComponents.length > 1) {
+            headers = messageComponents[1];
+        }
+    }
+
+    public String getRequestLine() {
+        return requestLine;
+    }
+
+    public String getHeaders(){
+        return headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+}
---
+++ test/com/lisahamm/RequestParserTest.java
@@ -0,0 +1,55 @@
+package com.lisahamm;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class RequestParserTest {
+
+    @Test
+    public void testParseRequestWithOnlyRequestLine() throws Exception {
+        String httpRequest = "GET / HTTP/1.1";
+        RequestParser requestParser = new RequestParser(httpRequest);
+        requestParser.parseRequest();
+        assertEquals(httpRequest, requestParser.getRequestLine());
+    }
+
+    @Test
+    public void testParseRequestWithHeader() throws Exception {
+        String httpRequest = "GET / HTTP/1.1\r\nHost: 0.0.0.0";
+        RequestParser requestParser = new RequestParser(httpRequest);
+        requestParser.parseRequest();
+        assertEquals("GET / HTTP/1.1", requestParser.getRequestLine());
+        assertEquals("Host: 0.0.0.0", requestParser.getHeaders());
+    }
+//
+    @Test
+    public void testParseRequestWithBody() throws Exception {
+        String httpRequest = "GET / HTTP/1.1\r\n\r\nBody";
+        RequestParser requestParser = new RequestParser(httpRequest);
+        requestParser.parseRequest();
+        assertEquals("GET / HTTP/1.1", requestParser.getRequestLine());
+        assertEquals("Body", requestParser.getBody());
+    }
+
+    @Test
+    public void testParseRequestWithHeaderAndBody() throws Exception {
+        String httpRequest = "GET / HTTP/1.1\r\nHost: 0.0.0.0\r\n\r\nBody";
+        RequestParser requestParser = new RequestParser(httpRequest);
+        requestParser.parseRequest();
+        assertEquals("GET / HTTP/1.1", requestParser.getRequestLine());
+        assertEquals("Host: 0.0.0.0", requestParser.getHeaders());
+        assertEquals("Body", requestParser.getBody());
+    }
+
+    @Test
+    public void testParseRequestWithMultipleHeaders() throws Exception {
+        String httpRequest = "GET / HTTP/1.1\r\nHost: 0.0.0.0\r\nContent-Type: text/plain\r\n\r\nBody";
+        RequestParser requestParser = new RequestParser(httpRequest);
+        requestParser.parseRequest();
+        assertEquals("GET / HTTP/1.1", requestParser.getRequestLine());
+        assertTrue(requestParser.getHeaders().contains("Host: 0.0.0.0"));
+        assertTrue(requestParser.getHeaders().contains("Content-Type: text/plain"));
+        assertEquals("Body", requestParser.getBody());
+    }
+}
