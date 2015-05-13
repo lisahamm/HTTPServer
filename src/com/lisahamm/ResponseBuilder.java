dedf640d2dfb99bd28a34cc67e8d0a03174d316e
Lisa Hamm <lisalhamm@gmail.com>
2015-05-13 13:18:26 -0500

    Initial commit of ResponseBuilder class

--- src/com/lisahamm/ResponseBuilder.java
+++ src/com/lisahamm/ResponseBuilder.java
@@ -1,8 +1,44 @@
 package com.lisahamm;
 
-public interface ResponseBuilder {
-    void constructStatusLine(HTTPRequest request);
-    void constructHeaders(HTTPRequest request);
-    void constructBody(HTTPRequest request);
-    String getResponse();
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseBuilder {
+    private String httpVersion = "HTTP/1.1";
+    private String CRLF = "\r\n";
+    private String response;
+    private String statusLine;
+    private String headers;
+    private String body;
+
+    public void addStatusLine(String statusCode) {
+        String status = responseStatuses().get(statusCode);
+        statusLine = httpVersion + status + CRLF;
+    }
+
+    public void addHeader(String header) {
+        headers += header + CRLF;
+    }
+
+    public void addBody(String bodyContent) {
+        body = CRLF + bodyContent;
+    }
+
+    public String getResponse() {
+        constructResponse();
+        return response;
+    }
+
+    private void constructResponse() {
+        response = statusLine + headers + body;
+    }
+
+    private Map<String, String> responseStatuses() {
+        Map<String, String> statuses = new HashMap<>();
+        statuses.put("200", "200 OK");
+        statuses.put("302", "302 Found");
+        statuses.put("404", "404 Not Found");
+        statuses.put("405", "405 Method Not Allowed");
+        return statuses;
+    }
 }
