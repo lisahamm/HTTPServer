3d4365cb3607c4b4838bd51ab7274e8349a1c5d5
Lisa Hamm <lisalhamm@gmail.com>
2015-05-18 13:02:50 -0500

    FileHandler class now takes FileManager as argument

--- src/com/lisahamm/FileHandler.java
+++ src/com/lisahamm/FileHandler.java
@@ -1,27 +1,24 @@
 package com.lisahamm;
 
-import java.io.*;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class FileHandler implements RequestHandler {
-    private String pathToPublicDirectory = "./../cob_spec/public";
+    private FileManager fileManager;
 
+    public FileHandler(FileManager fileManager) {
+        this.fileManager = fileManager;
+    }
 
     public boolean handle(HTTPRequest request, ResponseBuilder response) {
         String requestMethod = request.getRequestMethod();
         String requestURI = request.getRequestURI();
+        String fileName = requestURI.substring(1);
 
-        if (fileExistsInDirectory(requestURI)) {
+        if (fileManager.isFileFound(fileName)) {
             switch(requestMethod) {
                 case "GET":
                     response.addStatusLine("200");
-                    String mimeType = getContentType(requestURI);
+                    String mimeType = fileManager.getContentType(requestURI);
                     response.addHeader("Content-Type: " + mimeType);
-                    response.addBody(buildBodyContentFromURI(requestURI));
+                    response.addBody(fileManager.getFileContents(requestURI));
                     break;
                 default:
                     response.addStatusLine("405");
@@ -30,53 +27,5 @@ public class FileHandler implements RequestHandler {
             return true;
         }
         return false;
-    }
-
-    private File[] buildDirectoryContents() {
-        File f = new File(pathToPublicDirectory);
-        File[] files = f.listFiles();
-        return files;
-    }
-
-    private byte[] buildBodyContentFromURI(String requestURI) {
-        String uriPath = pathToPublicDirectory + requestURI;
-        byte[] data = "".getBytes();
-        try {
-            data = Files.readAllBytes(Paths.get(uriPath));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return data;
-    }
-
-    private String getContentType(String requestURI) {
-        File file = new File(pathToPublicDirectory + requestURI);
-        String mimeType = "";
-        InputStream is = null;
-        try {
-            is = new BufferedInputStream(new FileInputStream(file));
-            mimeType = URLConnection.guessContentTypeFromStream(is);
-            if (mimeType == null) {
-                mimeType = "text/plain";
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return mimeType;
-    }
-
-    private boolean fileExistsInDirectory(String requestURI) {
-        boolean exists = false;
-
-        File[] directoryFiles = buildDirectoryContents();
-
-        for(File file : directoryFiles) {
-            if (("/" + file.getName()).equals(requestURI)) {
-                exists = true;
-            }
-        }
-        return exists;
     }
 }
--- test/com/lisahamm/FileHandlerTest.java
+++ test/com/lisahamm/FileHandlerTest.java
@@ -1,7 +1,90 @@
 package com.lisahamm;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by lisahamm on 5/18/15.
  */
 public class FileHandlerTest {
+    private RequestHandler fileHandler;
+    private ResponseBuilder response;
+    private String pathToPublicDirectory = "./../cob_spec/public";
+
+    @Before
+    public void setUp() throws Exception {
+        response = new ResponseBuilder();
+        FileManager fileManager = new FileManager(pathToPublicDirectory);
+        fileHandler = new FileHandler(fileManager);
+    }
+
+    @Test
+    public void testItHandlesRequestForFileInPublicDirectory() throws Exception {
+        HTTPRequest validRequest = generateRequest("GET", "/file1");
+        boolean isHandled = fileHandler.handle(validRequest, response);
+        assertTrue(response.getResponseHeader().contains("HTTP/1.1 200 OK\r\n"));
+        assertTrue(response.getResponseHeader().contains("Content-Type: text/plain\r\n"));
+        assertTrue(response.getBody().length > 0);
+        assertTrue(isHandled);
+    }
+
+    @Test
+    public void testHandlesResponseForImageInPublicDirectory() throws Exception {
+        HTTPRequest validRequest = generateRequest("GET", "/image.gif");
+        boolean isHandled = fileHandler.handle(validRequest, response);
+        assertTrue(response.getResponseHeader().contains("HTTP/1.1 200 OK\r\n"));
+        assertTrue(response.getResponseHeader().contains("Content-Type: image/gif\r\n"));
+        assertTrue(response.getBody().length > 0);
+        assertTrue(isHandled);
+    }
+
+    @Test
+    public void testItHandlesRequestForMethodNotAllowed() throws Exception {
+        HTTPRequest invalidRequest = generateRequest("POST", "/file1");
+        boolean isHandled = fileHandler.handle(invalidRequest, response);
+        assertTrue(response.getResponseHeader().contains("HTTP/1.1 405 Method Not Allowed"));
+        assertTrue(isHandled);
+    }
+
+    @Test
+    public void testDoesNotHandleRequestIfUriDoesNotExistInDirectory() throws Exception {
+        HTTPRequest invalidRequest = generateRequest("GET", "/file1000");
+        boolean isHandled = fileHandler.handle(invalidRequest, response);
+        assertFalse(isHandled);
+    }
+
+    private HTTPRequest generateRequest(String method, String uri) {
+        Map<String, String> parsedComponents = new HashMap<>();
+        parsedComponents.put("requestMethod", method.toUpperCase());
+        parsedComponents.put("requestURI", uri);
+        parsedComponents.put("httpVersion", "HTTP/1.1");
+        return new HTTPRequest(parsedComponents);
+    }
+
+    public class MockFileManager implements DirectoryManager {
+        public boolean isFileFound(String fileName) {
+            if (fileName.equals("file1")) {
+                return true;
+            }
+            return false;
+        }
+
+        public byte[] getFileContents(String requestURI) {
+            return "body".getBytes();
+        }
+
+        public String getContentType(String requestURI) {
+            if (requestURI.contains("image.gif")) {
+                return "image/gif";
+            }
+            return "text/plain";
+        }
+    }
 }
