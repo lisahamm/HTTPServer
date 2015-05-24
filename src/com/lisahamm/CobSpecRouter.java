070be967fe0a5f71efdce589f8d6e8cfbd85993d
Lisa Hamm <lisalhamm@gmail.com>
2015-05-24 19:32:07 -0400

    Added Application-specific router

---
+++ src/com/lisahamm/CobSpecRouter.java
@@ -0,0 +1,35 @@
+package com.lisahamm;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CobSpecRouter implements Router {
+    private List<RequestHandler> requestHandlers = new LinkedList<>();
+
+    public CobSpecRouter() {
+        registerHandlers();
+    }
+
+    public void invoke(HTTPRequest request, ResponseBuilder response) {
+        boolean handled = false;
+        for (RequestHandler requestHandler : requestHandlers) {
+            if (handled) {
+                break;
+            }
+            handled = requestHandler.handle(request, response);
+        }
+    }
+
+    public void registerHandler(RequestHandler handler) {
+        requestHandlers.add(handler);
+    }
+
+    private void registerHandlers() {
+        String pathToPublicDirectory = "./../cob_spec/public";
+        registerHandler(new RootHandler(new PublicFileManager(pathToPublicDirectory)));
+        registerHandler(new FormHandler());
+        registerHandler(new MethodOptionsHandler());
+        registerHandler(new FileHandler(new PublicFileManager(pathToPublicDirectory)));
+        registerHandler(new RedirectHandler());
+        registerHandler(new ParametersHandler());
+    }}
