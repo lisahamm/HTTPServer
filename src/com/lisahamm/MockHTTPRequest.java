5ab2caefe6656988ee80a75b62b15597c82f916d
Lisa Hamm <lisalhamm@gmail.com>
2015-05-26 14:53:40 -0500

    Initial commit of MockHTTPRequest class

---
+++ src/com/lisahamm/MockHTTPRequest.java
@@ -0,0 +1,33 @@
+package com.lisahamm;
+
+import java.util.Map;
+
+public class MockHTTPRequest implements Request {
+    public String requestMethod = null;
+    public String requestURI = null;
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public String getRequestURI() {
+        return requestURI;
+    }
+
+    public String getHTTPVersion() {
+        return "HTTP/1.1";
+    }
+
+    public String getHeaders() {
+        return null;
+    }
+
+    public String getBody() {
+        return null;
+    }
+
+    public Map<String, String> getParams() {
+        return null;
+    }
+
+}
