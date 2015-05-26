a259eed74848333cf56c227b67358ccef89456a9
Lisa Hamm <lisalhamm@gmail.com>
2015-05-26 14:52:58 -0500

    Pulled out hard-coding of allow header string and status code into variables

--- src/com/lisahamm/MethodOptionsHandler.java
+++ src/com/lisahamm/MethodOptionsHandler.java
@@ -2,6 +2,9 @@ package com.lisahamm;
 
 public class MethodOptionsHandler implements RequestHandler {
     public static final String uri = "/method_options";
+    private static String code200 = "200";
+    private static String code405 = "405";
+    private static String allowHeader = "Allow: GET,HEAD,POST,OPTIONS,PUT";
 
     public boolean handle(Request request, ResponseBuilder response) {
         String requestMethod = request.getRequestMethod();
@@ -10,25 +13,25 @@ public class MethodOptionsHandler implements RequestHandler {
         if (requestURI.equals(this.uri)) {
             switch(requestMethod) {
                 case "GET":
-                    response.addStatusLine("200");
+                    response.addStatusLine(code200);
                     response.addHeader("Content-Type: text/plain");
-                    response.addHeader("Allow: GET, HEAD, POST, OPTIONS, PUT");
+                    response.addHeader(allowHeader);
                     break;
                 case "POST":
-                    response.addStatusLine("200");
-                    response.addHeader("Allow: GET, HEAD, POST, OPTIONS, PUT");
+                    response.addStatusLine(code200);
+                    response.addHeader(allowHeader);
                     break;
                 case "PUT":
-                    response.addStatusLine("200");
-                    response.addHeader("Allow: GET, HEAD, POST, OPTIONS, PUT");
+                    response.addStatusLine(code200);
+                    response.addHeader(allowHeader);
                     break;
                 case "OPTIONS":
-                    response.addStatusLine("200");
-                    response.addHeader("Allow: GET,HEAD,POST,OPTIONS,PUT");
+                    response.addStatusLine(code200);
+                    response.addHeader(allowHeader);
                     break;
                 default:
-                    response.addStatusLine("405");
-                    response.addHeader("Allow: GET, HEAD, POST, OPTIONS, PUT");
+                    response.addStatusLine(code405);
+                    response.addHeader(allowHeader);
                     break;
             }
             return true;
