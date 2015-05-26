303fdbdab0425da0f509980593658bd2130b6187
Lisa Hamm <lisalhamm@gmail.com>
2015-05-26 11:42:48 -0500

    Removed unused fields

--- src/com/lisahamm/RedirectHandler.java
+++ src/com/lisahamm/RedirectHandler.java
@@ -4,11 +4,7 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class RedirectHandler implements RequestHandler {
-    private String pathToPublicDirectory = "./../cob_spec/public";
-    private Charset charset = StandardCharsets.UTF_8;
-
     public static final String uri = "/redirect";
-
 
     public boolean handle(Request request, ResponseBuilder response) {
         String requestMethod = request.getRequestMethod();
