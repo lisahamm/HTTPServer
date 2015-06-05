5c556a54b0017053b8dda1e185e9ff7847b75a55
Lisa Hamm <lisalhamm@gmail.com>
2015-06-05 09:38:29 -0500

    White space

--- src/com/lisahamm/ClientHandler.java
+++ src/com/lisahamm/ClientHandler.java
@@ -24,6 +24,7 @@ public class ClientHandler extends Thread {
             } while (in.ready());
 
             if (rawRequest.length() > 1) {
+
                 RequestParser parser = new RequestParser();
                 HTTPRequest request = parser.generateParsedRequest(rawRequest.toString());
                 ResponseBuilder responseBuilder = new ResponseBuilder();
