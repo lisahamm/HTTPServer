76f08e2d6c346297f12ff7bc35f7520659ae35a4
Lisa Hamm <lisalhamm@gmail.com>
2015-06-05 09:45:21 -0500

    Added test for partial content request

--- test/com/lisahamm/application/handlers/FileHandlerTest.java
+++ test/com/lisahamm/application/handlers/FileHandlerTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -16,17 +17,19 @@ import static org.junit.Assert.assertTrue;
 public class FileHandlerTest {
     private RequestHandler fileHandler;
     private ResponseBuilder response;
+    private Map<String, String> headers;
 
     @Before
     public void setUp() throws Exception {
+        headers = new HashMap<>();
         response = new ResponseBuilder();
         FileManager fileManager = new MockFileManager();
         fileHandler = new FileHandler(fileManager);
     }
 
     @Test
-    public void testItHandlesRequestForFileInPublicDirectory() throws Exception {
-        Request validRequest = generateRequest("GET", "/file1");
+    public void testItHandlesRequestForFullFileInPublicDirectory() throws Exception {
+        Request validRequest = generateRequest("GET", "/file1", headers);
         boolean isHandled = fileHandler.handle(validRequest, response);
         assertTrue(response.getResponseHeader().contains("HTTP/1.1 200 OK\r\n"));
         assertTrue(response.getResponseHeader().contains("Content-Type: text/plain\r\n"));
@@ -35,8 +38,21 @@ public class FileHandlerTest {
     }
 
     @Test
+    public void testItHandlesRequestForPartialFileContentsInPublicDirectory() throws Exception {
+        headers.put("Range", "bytes=0-4");
+        Request validRequest = generateRequest("GET", "/file1", headers);
+
+        boolean isHandled = fileHandler.handle(validRequest, response);
+
+        assertTrue(response.getResponseHeader().contains("HTTP/1.1 206 Partial Content\r\n"));
+        assertTrue(response.getResponseHeader().contains("Content-Type: text/plain\r\n"));
+        assertEquals(5, response.getBody().length);
+        assertTrue(isHandled);
+    }
+
+    @Test
     public void testHandlesResponseForImageInPublicDirectory() throws Exception {
-        Request validRequest = generateRequest("GET", "/image.gif");
+        Request validRequest = generateRequest("GET", "/image.gif", headers);
         boolean isHandled = fileHandler.handle(validRequest, response);
         assertTrue(response.getResponseHeader().contains("HTTP/1.1 200 OK\r\n"));
         assertTrue(response.getResponseHeader().contains("Content-Type: image/gif\r\n"));
@@ -46,7 +62,7 @@ public class FileHandlerTest {
 
     @Test
     public void testItHandlesRequestForMethodNotAllowed() throws Exception {
-        Request invalidRequest = generateRequest("POST", "/file1");
+        Request invalidRequest = generateRequest("POST", "/file1", headers);
         boolean isHandled = fileHandler.handle(invalidRequest, response);
         assertTrue(response.getResponseHeader().contains("HTTP/1.1 405 Method Not Allowed"));
         assertTrue(isHandled);
@@ -54,15 +70,16 @@ public class FileHandlerTest {
 
     @Test
     public void testDoesNotHandleRequestIfUriDoesNotExistInDirectory() throws Exception {
-        Request invalidRequest = generateRequest("GET", "/file1000");
+        Request invalidRequest = generateRequest("GET", "/file1000", headers);
         boolean isHandled = fileHandler.handle(invalidRequest, response);
         assertFalse(isHandled);
     }
 
-    private MockHTTPRequest generateRequest(String method, String uri) {
+    private MockHTTPRequest generateRequest(String method, String uri, Map<String, String> headers) {
         MockHTTPRequest request = new MockHTTPRequest();
         request.requestMethod = method.toUpperCase();
         request.requestURI = uri;
+        request.headers = headers;
         return request;
     }
 
@@ -75,7 +92,11 @@ public class FileHandlerTest {
         }
 
         public byte[] getFileContents(String requestURI) {
-            return "body".getBytes();
+            return "file1 contents".getBytes();
+        }
+
+        public byte[] getPartialFileContents(String requestURI, String range) {
+            return "file1".getBytes();
         }
 
         public String getContentType(String requestURI) {
