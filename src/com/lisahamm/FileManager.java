4836f427276f539e12136f7f26ce2fa893b956b8
Lisa Hamm <lisalhamm@gmail.com>
2015-06-05 09:39:24 -0500

    Added #getPartialFileContents method to the FileManager interface

--- src/com/lisahamm/FileManager.java
+++ src/com/lisahamm/FileManager.java
@@ -3,5 +3,6 @@ package com.lisahamm;
 public interface FileManager {
     boolean isFileFound(String fileName);
     byte[] getFileContents(String requestURI);
+    byte[] getPartialFileContents(String requestURI, String range);
     String getContentType(String requestURI);
 }
