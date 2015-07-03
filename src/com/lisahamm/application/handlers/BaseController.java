88ae34f26650b7401e70345f668b88ed75c86acb
Lisa Hamm <lisalhamm@gmail.com>
2015-07-03 18:32:27 -0400

    Change BaseController to abstract and delete spec

--- src/com/lisahamm/application/handlers/BaseController.java
+++ src/com/lisahamm/application/handlers/BaseController.java
@@ -1,8 +1,9 @@
 package com.lisahamm.application.handlers;
 
 import com.lisahamm.Request;
+import com.lisahamm.ResponseBuilder;
 
-public class BaseController {
+public abstract class BaseController {
     private String uri;
 
     public BaseController(String uri) {
@@ -12,4 +13,36 @@ public class BaseController {
     public boolean itShouldExecute(Request request) {
         return uri.equals(request.getRequestURI());
     }
+
+    public void execute(Request request, ResponseBuilder response) {
+        switch (request.getRequestMethod()) {
+            case "GET":
+                handleGet(request, response);
+                break;
+            case "POST":
+                handlePost(request, response);
+                break;
+            case "PUT":
+                handlePut(request, response);
+                break;
+            case "PATCH":
+                handlePatch(request, response);
+                break;
+            case "DELETE":
+                handleDelete(request, response);
+                break;
+            case "OPTIONS":
+                handleOptions(request, response);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void handleGet(Request request, ResponseBuilder response){};
+    private void handlePost(Request request, ResponseBuilder response){};
+    private void handlePut(Request request, ResponseBuilder response){};
+    private void handlePatch(Request request, ResponseBuilder response){};
+    private void handleDelete(Request request, ResponseBuilder response){};
+    private void handleOptions(Request request, ResponseBuilder response){};
 }
