766e57e8222391bdda321b2d9d30c8996e674ae2
Lisa Hamm <lisalhamm@gmail.com>
2015-07-07 13:08:19 -0500

    Modify settings to reflect gradle structure

--- src/main/java/server/core/configuration/Settings.java
+++ src/main/java/server/core/configuration/Settings.java
@@ -1,6 +1,6 @@
 package server.core.configuration;
 
 public class Settings {
-    public static String publicDirectory = System.getProperty("user.dir") + "/public";
-    public static String dataStorage = System.getProperty("user.dir") + "/data";
+    public static String publicDirectory = "src/main/resources/cobspec/";
+    public static String dataStorage = "src/main/resources/database/";
 }
