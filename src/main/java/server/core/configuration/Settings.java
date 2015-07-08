b3f7a5bd0c5f8689ef1ef057b6b8a78b73894150
Lisa Hamm <lisalhamm@gmail.com>
2015-07-07 19:27:28 -0500

    Remove unnecessary database path

--- src/main/java/server/core/configuration/Settings.java
+++ src/main/java/server/core/configuration/Settings.java
@@ -1,6 +1,6 @@
 package server.core.configuration;
 
 public class Settings {
-    public static String publicDirectory = "src/main/resources/cobspec/";
-    public static String dataStorage = "src/main/resources/database/";
+    public static int portNumber = 5000;
+    public static String publicDirectory = "src/main/resources/cobspec";
 }
